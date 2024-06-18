93bf222b7413e4802f784ad4c055113a31f70a69
Vivekyadav8314 <royvivek02u@gmail.com>
2024-06-18 20:00:10 +0530

    Add files via upload

---
+++ logicaloper.c
@@ -0,0 +1,31 @@
+//C program to demonstrate logical operators
+
+
+#include<stdio.h>
+
+int main()
+{
+
+	int a=5, b=5, c=10, result;
+
+	result=(a==b) && (c>b);
+
+	printf("(a==b) && (c> b) is %d \n ", result);//1
+
+	result=(a==b) && (c<b);
+	printf("(a==b) && (c<b) is %d\n", result);//o
+
+	result=(a==b) || (c<b);
+	printf("(a==b) && (c<b) is %d\n", result);//1
+
+	result =(a!=b) || (c<b);
+	printf("(a!=b) is %d \n", result);//0
+					  //
+        result=!(a!=b);
+	printf("!(a!=b) is %d\n", result); //--> 0 !0 is 1
+
+	result =!(a==b);
+	printf("!(a==b) is %d \n ", result);
+
+	return 0;
+}
